879a42a4beb60965216e5cfef8111c38942d03a9
Markus Hoffrogge <mhoffrogge@gmail.com>
2023-03-21 23:38:12 +0100

    Move "Repeat Build Options" logs to start-end.sh
    - cli-build.sh, start-end.sh:
      - move function function produce_repeat_args_array() to start-end.sh
      - add $WHAT as first arg to produce_repeat_args_array() if
        $WHAT is not empty
      - move early display log of "Repeat Build Options" to
        function main_default_start_build()
      - move last log of "Repeat Build Options" to
        function main_default_end_build()
      - add last log of "Repeat Build Options" to
        LOG_SECTION="repeat_build_options"

--- lib/functions/main/start-end.sh
+++ lib/functions/main/start-end.sh
@@ -10,6 +10,10 @@
 # Common start/end build functions. Used by the default build and others
 
 function main_default_start_build() {
+	# 1x: show interactively-selected as soon as possible, after config
+	produce_repeat_args_array
+	display_alert "Repeat Build Options (early)" "${repeat_args[*]}" "ext"
+
 	wait_for_disk_sync "before starting build" # fsync, wait for disk to sync, and then continue. alert user if takes too long.
 
 	# Check that WORKDIR_BASE_TMP exists; if not, create it.
@@ -76,6 +80,9 @@ function main_default_end_build() {
 	# display_alert in its own logging section.
 	LOG_SECTION="runtime_total" do_with_logging display_alert "Runtime" "$(printf "%d:%02d min" $((runtime_seconds / 60)) $((runtime_seconds % 60)))" "info"
 
+	produce_repeat_args_array
+	LOG_SECTION="repeat_build_options" do_with_logging display_alert "Repeat Build Options" "${repeat_args[*]}" "ext" # * = expand array, space delimited, single-word.
+
 	return 0
 }
 
@@ -94,4 +101,22 @@ function trap_handler_cleanup_workdir() {
 			# @TODO: tmpfs might just be unmounted, though.
 		fi
 	fi
+}
+
+function produce_repeat_args_array() {
+	# Make it easy to repeat build by displaying build options used. Prepare array.
+	declare -a -g repeat_args=("./compile.sh")
+	# @TODO: missing the config file name, if any.
+	# @TODO: missing the original cli command, if different from build/docker
+	[[ -n ${WHAT} ]] && repeat_args+=("${WHAT}")
+	[[ -n ${BOARD} ]] && repeat_args+=("BOARD=${BOARD}")
+	[[ -n ${BRANCH} ]] && repeat_args+=("BRANCH=${BRANCH}")
+	[[ -n ${RELEASE} ]] && repeat_args+=("RELEASE=${RELEASE}")
+	[[ -n ${BUILD_MINIMAL} ]] && repeat_args+=("BUILD_MINIMAL=${BUILD_MINIMAL}")
+	[[ -n ${BUILD_DESKTOP} ]] && repeat_args+=("BUILD_DESKTOP=${BUILD_DESKTOP}")
+	[[ -n ${KERNEL_CONFIGURE} ]] && repeat_args+=("KERNEL_CONFIGURE=${KERNEL_CONFIGURE}")
+	[[ -n ${DESKTOP_ENVIRONMENT} ]] && repeat_args+=("DESKTOP_ENVIRONMENT=${DESKTOP_ENVIRONMENT}")
+	[[ -n ${DESKTOP_ENVIRONMENT_CONFIG_NAME} ]] && repeat_args+=("DESKTOP_ENVIRONMENT_CONFIG_NAME=${DESKTOP_ENVIRONMENT_CONFIG_NAME}")
+	[[ -n ${DESKTOP_APPGROUPS_SELECTED} ]] && repeat_args+=("DESKTOP_APPGROUPS_SELECTED=\"${DESKTOP_APPGROUPS_SELECTED:-"none"}\"")
+	[[ -n ${COMPRESS_OUTPUTIMAGE} ]] && repeat_args+=("COMPRESS_OUTPUTIMAGE=${COMPRESS_OUTPUTIMAGE}")
 }
